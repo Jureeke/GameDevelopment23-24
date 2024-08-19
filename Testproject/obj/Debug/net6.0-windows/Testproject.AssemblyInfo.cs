c16b59a07752e5e3d43cb2d033ce8a8902f09266
Vincent Juré <vincent.jure@gmail.com>
2024-08-19 12:35:37 +0200

    Cache troep

--- Testproject/obj/Debug/net6.0-windows/Testproject.AssemblyInfo.cs
+++ Testproject/obj/Debug/net6.0-windows/Testproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179ca0b3550f0443aab3168fafd3d72fb09d6c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

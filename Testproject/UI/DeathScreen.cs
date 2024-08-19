7d3dd8166b473c66a35bef3c1aa829fd6bc0e37e
Vincent Juré <vincent.jure@gmail.com>
2024-08-19 15:18:57 +0200

    Add better images

--- Testproject/UI/DeathScreen.cs
+++ Testproject/UI/DeathScreen.cs
@@ -10,26 +10,22 @@ namespace Testproject.UI
         private SpriteFont _font;
 
         private Texture2D _plainTexture;
+        private Texture2D _deathImage;
 
-        private Text _titleText;
         private Button _exitButton;
 
         public DeathScreen(GameManager game)
         {
             _game = game;
             _plainTexture = new Texture2D(_game.RootGame.GraphicsDevice, 1, 1);
-            _plainTexture.SetData(new[] { Color.White });
+            _plainTexture.SetData(new[] { Color.Black });
+            _deathImage = _game.RootGame.Content.Load<Texture2D>("YouDied");
 
             _font = _game.RootGame.Content.Load<SpriteFont>("font");
 
-            // UI element initialization
-            Vector2 titleTextPosition = new Vector2(_game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth / 2,
-                _game.RootGame.GraphicsDeviceManager.PreferredBackBufferHeight / 4);
-            _titleText = new Text("You Died!", titleTextPosition, _font, 5);
-
-            Vector2 startButtonPosition = new Vector2(_game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth / 2,
+            Vector2 buttonPosition = new Vector2(_game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth / 2,
                 _game.RootGame.GraphicsDeviceManager.PreferredBackBufferHeight / 2);
-            _exitButton = new Button("Exit", startButtonPosition, _font, _plainTexture, () => 
+            _exitButton = new Button("Exit", buttonPosition, _game.RootGame.Content.Load<SpriteFont>("font"), _plainTexture, () =>
             {
                 System.Environment.Exit(0);
             }, 5);
@@ -43,10 +39,15 @@ namespace Testproject.UI
         public void Draw(SpriteBatch spriteBatch)
         {
             // Draw background
-            spriteBatch.Draw(_plainTexture, new Rectangle(0, 0, _game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth, _game.RootGame.GraphicsDeviceManager.PreferredBackBufferHeight), Color.Black);
+            spriteBatch.Draw(_plainTexture, new Rectangle(0, 0, _game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth, _game.RootGame.GraphicsDeviceManager.PreferredBackBufferHeight), Color.White);
 
-            // UI elements
-            _titleText.Draw(spriteBatch);
+            // Draw win image
+            Vector2 deathImagePosition = new Vector2(_game.RootGame.GraphicsDeviceManager.PreferredBackBufferWidth / 2,
+                _game.RootGame.GraphicsDeviceManager.PreferredBackBufferHeight / 4);
+            Vector2 deathImageOrigin = new Vector2(_deathImage.Width / 2, _deathImage.Height / 2);
+            spriteBatch.Draw(_deathImage, deathImagePosition, null, Color.White, 0f, deathImageOrigin, 1f, SpriteEffects.None, 0f);
+
+            // Draw exit button
             _exitButton.Draw(spriteBatch);
         }
     }
